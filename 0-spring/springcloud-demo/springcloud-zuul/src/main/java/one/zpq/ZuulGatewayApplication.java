27592582900d8154a855f4000542b0da1be2b49c
zhangchaopei <2251745121@qq.com>
2023-10-20 15:40:28 +0800

    zuul 启动类命名修改

--- 0-spring/springcloud-demo/springcloud-zuul/src/main/java/one/zpq/ZuulGatewayApplication.java
+++ 0-spring/springcloud-demo/springcloud-zuul/src/main/java/one/zpq/ZuulGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class Application {
+public class ZuulGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(ZuulGatewayApplication.class,args);
     }
 }
