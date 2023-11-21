8501199d2b7049c71b2ae564f9a4e0240c30844e
zpq-taishi <2251745121@qq.com>
2023-11-21 22:03:03 +0800

    dubbo3 RpcContext 尝试

--- dubbo-demo/dubbo-boot-demo/dubbo-demo-service/src/main/java/one/zpq/dubbodemo/dubbo/service/DemoServiceImpl.java
+++ dubbo-demo/dubbo-boot-demo/dubbo-demo-service/src/main/java/one/zpq/dubbodemo/dubbo/service/DemoServiceImpl.java
@@ -1,14 +1,20 @@
 package one.zpq.dubbodemo.dubbo.service;
 
+import com.alibaba.fastjson2.JSON;
 import one.zpq.dubbodemo.dubbo.api.DemoService;
 
 import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.rpc.RpcContext;
+
+import java.util.Map;
 
 @DubboService
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public String sayHello(String name) {
+        Map<String, Object> serverAttachments = RpcContext.getServerAttachment().getObjectAttachments();
+        System.out.println("ContextService serverAttachments:" + JSON.toJSONString(serverAttachments));
         return "Hello " + name;
     }
 }
--- dubbo-demo/dubbo-boot-demo/dubbo-demo-user/src/main/java/one/zpq/service/TestService.java
+++ dubbo-demo/dubbo-boot-demo/dubbo-demo-user/src/main/java/one/zpq/service/TestService.java
@@ -3,6 +3,7 @@ package one.zpq.service;
 
 import one.zpq.dubbodemo.dubbo.api.DemoService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.rpc.RpcContext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ public class TestService implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        RpcContext.getClientAttachment().setAttachment("clientKey1","clientValue1");
         String result = demoService.sayHello("world");
         System.out.println("Receive result ======> " + result);
     }
