c8ade1ac731ab23327695f482d497eaea6bbe004
zpq-taishi <2251745121@qq.com>
2023-11-26 20:20:19 +0800

    R dubbo3 RpcContext 尝试[修改日志记录方式]

--- dubbo-demo/dubbo-boot-demo/dubbo-demo-service/src/main/java/one/zpq/dubbodemo/dubbo/service/DemoServiceImpl.java
+++ dubbo-demo/dubbo-boot-demo/dubbo-demo-service/src/main/java/one/zpq/dubbodemo/dubbo/service/DemoServiceImpl.java
@@ -3,6 +3,8 @@ package one.zpq.dubbodemo.dubbo.service;
 import com.alibaba.fastjson2.JSON;
 import one.zpq.dubbodemo.dubbo.api.DemoService;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.rpc.RpcContext;
 
@@ -11,10 +13,12 @@ import java.util.Map;
 @DubboService
 public class DemoServiceImpl implements DemoService {
 
+    private final Log log = LogFactory.getLog(DemoServiceImpl.class);
+
     @Override
     public String sayHello(String name) {
         Map<String, Object> serverAttachments = RpcContext.getServerAttachment().getObjectAttachments();
-        System.out.println("ContextService serverAttachments:" + JSON.toJSONString(serverAttachments));
+        log.info("ContextService serverAttachments:" + JSON.toJSONString(serverAttachments));
         return "Hello " + name;
     }
 }
